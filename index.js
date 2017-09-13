b132fd4e4f4e0309d750feea71b409f2499ddc84
Trevor Seitz <trevinla@gmail.com>
2017-09-13 16:58:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase) {
     return "YES INDEED!";
-  }else {
+  }else if {
     if (string === string.toLowerCase) {
       return "I can't hear you!"
     }
